e1b253834e657a1670412703f319da356825d23c
g22919ks <g22919ks@gm.tsuda.ac.jp>
2025-04-25 16:58:22 +0900

    Add: Java program to print favorite food and reason

---
+++ FavoriteFood.java
@@ -0,0 +1,9 @@
+public class FavoriteFood{
+    public static void main(String[] args){
+        String food = "さつまいも";
+        String reason = "何もしなくても甘い";
+
+        System.out.println(food);
+        System.out.println(reason);
+    }
+}
