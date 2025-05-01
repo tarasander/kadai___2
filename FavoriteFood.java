ef761001f7e9f3a5cb2ec4dc4c0e628a66d55174
g22919ks <g22919ks@gm.tsuda.ac.jp>
2025-05-01 23:49:32 +0900

    Add another line about favorite place

--- FavoriteFood.java
+++ FavoriteFood.java
@@ -1,9 +1,16 @@
 public class FavoriteFood{
+    /**
+     * @param args
+     */
     public static void main(String[] args){
         String food = "さつまいも";
         String reason = "何もしなくても甘い";
+        String place = "アラビアンコースト";
+        String reazon2 = "日本のディズニーとアラジンの世界観とシンドバッド・ストーリーブック・ヴォヤッジが好きだから。";
 
-        System.out.println(food);
+        System.out.println("好きな食べ物は" + food);
         System.out.println(reason);
+        System.out.println("好きな場所は" + place);
+        System.out.println(reazon2);
     }
 }
